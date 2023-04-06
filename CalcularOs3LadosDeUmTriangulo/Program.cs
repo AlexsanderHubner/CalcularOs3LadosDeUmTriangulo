9e69885d8f93f65dcc0a35751cfd8ccdcb6e4175
tomat <tomat@DESKTOP-0VULJ7N>
2023-04-06 14:57:00 -0300

    Add project files.

---
+++ CalcularOs3LadosDeUmTriangulo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace CalcularOs3LadosDeUmTriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo resultado = new Triangulo();
+
+            resultado.x = 12;
+            resultado.y = 7;
+            resultado.z = 14;
+
+            Console.WriteLine(resultado.ResultadoTriangulo());
+
+        }
+    }
+}
---
+++ CalcularOs3LadosDeUmTriangulo/Triangulo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularOs3LadosDeUmTriangulo
+{
+    internal class Triangulo
+    {
+        public double x;
+        public double y;
+        public double z;
+
+        public string ResultadoTriangulo()
+        {
+            if (x + y > z && x + z > y && y + z > x)
+            {
+                
+                if (x == y && x == z)
+                {
+                    return "É um triângulo equilátero.";
+                }
+                else if (x == y || x == z || y == z)
+                {
+                    return "É um triângulo isósceles.";
+                }
+                else
+                {
+                    return "É um triângulo escaleno.";
+                }
+            }
+            else
+            {
+                return "Os valores informados não formam um triângulo.";
+            }
+        }
+    }
+}
